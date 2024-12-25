add11e4d8f143aeff1f334b38da71ac80cd4d84b
nbcolak <159068555+nbcolak@users.noreply.github.com>
2024-12-25 14:18:52 +0300

    unit tests added

---
+++ Weatheryzer.Infrastructure/Migrations/20241225092657_AddTemperatureAndCondition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Weatheryzer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTemperatureAndCondition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "weatherdata",
+                schema: "public",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    city = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    data = table.Column<string>(type: "json", nullable: false),
+                    temperature_c = table.Column<double>(type: "double precision", nullable: false),
+                    weather_condition = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_weatherdata", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "weatherdata",
+                schema: "public");
+        }
+    }
+}
---
+++ Weatheryzer.Tests/CreateWeatherCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using Weatheryzer.Application.WeatherForecast.Command;
+using Weatheryzer.Domain.Entities;
+using Weatheryzer.Infrastructure;
+using Weatheryzer.Shared.Interfaces;
+using Xunit;
+
+namespace Weatheryzer.Tests;
+
+
+public class CreateWeatherCommandHandlerTests
+{
+    private readonly Mock<IUnitOfWork> _unitOfWorkMock;
+    private readonly CreateWeatherCommandHandler _handler;
+
+    public CreateWeatherCommandHandlerTests()
+    {
+        _unitOfWorkMock = new Mock<IUnitOfWork>();
+        _handler = new CreateWeatherCommandHandler(_unitOfWorkMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnResponse_WhenWeatherDataIsCreated()
+    {
+        // Arrange
+        var command = new CreateWeatherCommand(
+            City: "Istanbulxxx", 
+            TemperatureC: 25.5, 
+            Condition: "Sunny", 
+            Humidity: 75, 
+            WindSpeed: 15, 
+            AdditionalInfo: "Test Additional Info");
+        _unitOfWorkMock.Setup(u => u.Repository<WeatherData>().AddAsync(It.IsAny<WeatherData>()))
+            .Returns(Task.CompletedTask);
+        _unitOfWorkMock.Setup(u => u.SaveChangesAsync())
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var result = await _handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.True(result.Success);
+        Assert.Equal(201, result.StatusCode.Code);
+        Assert.Equal("Weather data created successfully.", result.Message);
+    }
+}
